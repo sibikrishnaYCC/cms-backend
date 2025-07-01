73d34e444dad1665ba4137881f1761954120e525
sibikrishnaYCC <sibikrishnakrishna@gmail.com>
2025-07-02 02:56:27 +0530

    auth check updating

--- Authentication/AuthCheck.js
+++ Authentication/AuthCheck.js
@@ -1,17 +1,23 @@
-
 const isAuthenticated = async (req, res) => {
+    try {
+        console.log('Session at /auth/authcheck:', req.session);
 
-
-    if (req.session.user) {
-        return res.status(200).json({
-            success: true,
-            user: req.session.user,
-        });
-    } else {
-        return res.status(401).json({
+        if (req.session.user) {
+            return res.status(200).json({
+                success: true,
+                user: req.session.user,
+            });
+        } else {
+            return res.status(401).json({
+                success: false,
+                message: 'Not authenticated.',
+            });
+        }
+    } catch (error) {
+        console.error('🔥 Error in authCheck:', error);
+        return res.status(500).json({
             success: false,
-            message: 'Not authenticated.'
-            
+            message: 'Server error during auth check.',
         });
     }
 };
